075af37db0e31c36d6590cd34939a73dd1b7fbc4
gniarchos <giannisnrch@gmail.com>
2023-07-15 02:29:25 +0300

    updates and ui fixes in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,16 +2,26 @@ import React from "react"
 import "./Footer.css"
 import api_logo from "../images/api-logo.png"
 import mdblist_logo from "../images/mdblist_logo.png"
+import { Icon } from "@iconify/react"
 
 export default function Footer() {
   return (
     <div className="footer-wrapper">
-      <h4 className="copyright">© 2022 - Giannis Niarchos</h4>
+      <div className="socials">
+        <a href="https://github.com/gniarchos/TVTime-ShowsTracker">
+          <Icon icon="mdi:github" className="social-icon" width={35} />
+        </a>
+        <a href="https://www.linkedin.com/in/giannis-niarchos/">
+          <Icon icon="mdi:linkedin" className="social-icon" width={35} />
+        </a>
+      </div>
+
+      <h4 className="copyright">Giannis Niarchos © 2022-2023 </h4>
 
       <div className="footer-api">
         <h4>Powered by </h4>
-        <img width={50} height={50} src={api_logo} alt="api-logo" />
-        <img width={50} height={50} src={mdblist_logo} alt="api-logo" />
+        <img className="api-images" src={api_logo} alt="api-logo" />
+        <img className="api-images" src={mdblist_logo} alt="api-logo" />
       </div>
     </div>
   )
