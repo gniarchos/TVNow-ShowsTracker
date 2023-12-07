891e49a7e9a4241b100b946625892136ae213175
gniarchos <giannisnrch@gmail.com>
2023-12-07 20:53:14 +0200

    Refactor: Updated People Component - Implemented changes in People.js and People.css files for enhanced functionality and appearance.

--- src/components/People/People.js
+++ src/components/People/People.js
@@ -1,35 +1,30 @@
 import React from "react"
-import { useLocation, useNavigate } from "react-router-dom"
+import { Link, useSearchParams, useNavigate } from "react-router-dom"
 import "./People.css"
-import Navbar from "../Navbar/Navbar"
-import Footer from "../Footer/Footer"
 import noImg from "../../images/no-image.png"
 import noFace from "../../images/no-face.png"
+import PuffLoader from "react-spinners/PuffLoader"
 
 export default function People() {
-  const isLoggedIn = true
-  const location = useLocation()
-  const navigate = useNavigate()
-
   const [personDetails, setPersonDetails] = React.useState([])
+
   const [personKnownFor, setPersonKnownFor] = React.useState([])
   const [isLoading, setIsLoading] = React.useState(true)
 
-  document.title = `TVTime | ${personDetails.name}`
+  const [searchParams, setSearchParams] = useSearchParams()
+  const param_person_name = searchParams.get("person_name")
+  const param_person_id = searchParams.get("person_id")
 
-  function goToShow(showID) {
-    fetch(
-      `https://api.themoviedb.org/3/tv/${showID}?api_key=${process.env.REACT_APP_THEMOVIEDB_API}&language=en-US&append_to_response=external_ids,videos,aggregate_credits,content_ratings,recommendations,similar,watch/providers,images`
-    )
-      .then((res) => res.json())
-      .then((data) => {
-        navigate("/overview", {
-          state: {
-            data: data,
-            userId: location.state.userId,
-          },
-        })
-      })
+  document.title = `TVTime | ${param_person_name}`
+
+  const navigate = useNavigate()
+  if (
+    param_person_name === "" ||
+    param_person_name === null ||
+    param_person_id === "" ||
+    param_person_id === null
+  ) {
+    navigate("/error404")
   }
 
   React.useEffect(() => {
@@ -37,24 +32,35 @@ export default function People() {
 
     setIsLoading(true)
 
-    Promise.all([
-      fetch(
-        `https://api.themoviedb.org/3/person/${location.state.person_id}/combined_credits?api_key=${process.env.REACT_APP_THEMOVIEDB_API}&language=en-US`
-      ),
-      fetch(
-        `https://api.themoviedb.org/3/person/${location.state.person_id}?api_key=${process.env.REACT_APP_THEMOVIEDB_API}&language=en-US&append_to_response=videos,images`
-      ),
-    ])
-      .then((responses) =>
-        Promise.all(responses.map((response) => response.json()))
+    const fetchPersonDetails = async () => {
+      return await fetch(
+        `https://api.themoviedb.org/3/person/${param_person_id}?api_key=${process.env.REACT_APP_THEMOVIEDB_API}&language=en-US&append_to_response=videos,images`
       )
-      .then((data) => {
-        // console.log(data)
-        setPersonKnownFor(data[0])
-        setPersonDetails(data[1])
+        .then((res) => res.json())
+        .then((data) => {
+          setPersonDetails(data)
+        })
+    }
+
+    const fetchPersonKnownFor = async () => {
+      return await fetch(
+        `https://api.themoviedb.org/3/person/${param_person_id}/combined_credits?api_key=${process.env.REACT_APP_THEMOVIEDB_API}&language=en-US`
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          setPersonKnownFor(data)
+        })
+    }
+
+    Promise.all([fetchPersonDetails(), fetchPersonKnownFor()])
+      .then(() =>
+        // console.log("All data is fetched.")
+        setIsLoading(false)
+      )
+      .catch((error) => {
+        console.log("Error fetching data:", error)
       })
-      .finally(() => setIsLoading(false))
-  }, [location])
+  }, [searchParams])
 
   const backgroundStyle = {
     backgroundImage:
@@ -66,7 +72,7 @@ export default function People() {
 
   var currentTime = new Date()
 
-  const in_cast_series = personKnownFor?.cast
+  let in_cast_series = personKnownFor?.cast
     ?.sort((a, b) => (a.first_air_date < b.first_air_date ? 1 : -1))
     .filter(
       (cast) =>
@@ -79,8 +85,8 @@ export default function People() {
     .map((cast) => {
       return (
         <div className="knownFor-content">
-          <div
-            onClick={() => goToShow(cast.id)}
+          <Link
+            to={`/show?show_name=${cast.name}&show_id=${cast.id}`}
             className="img-knownFor-container"
           >
             {cast.poster_path !== null ? (
@@ -95,7 +101,7 @@ export default function People() {
                 alt="not-found"
               />
             )}
-          </div>
+          </Link>
           <div className="knownFor-info-wrapper">
             <p className="knownFor-title">{cast.name}</p>
             <p className="knownFor-character">{cast.character}</p>
@@ -114,8 +120,8 @@ export default function People() {
     .map((cast) => {
       return (
         <div className="knownFor-content">
-          <div
-            onClick={() => goToShow(cast.id)}
+          <Link
+            to={`/show?show_name=${cast.name}&show_id=${cast.id}`}
             className="img-knownFor-container"
           >
             {cast.poster_path !== null ? (
@@ -130,7 +136,7 @@ export default function People() {
                 alt="not-found"
               />
             )}
-          </div>
+          </Link>
           <div className="knownFor-info-wrapper">
             <p className="knownFor-title">{cast.name}</p>
             <p>{cast.job}</p>
@@ -211,13 +217,21 @@ export default function People() {
       )
     })
 
+  if (isLoading) {
+    return (
+      <div className="spinner-div-home">
+        <PuffLoader color={"white"} size={100} />
+        <h3>Reloading Data...</h3>
+      </div>
+    )
+  }
+
   return (
     <div className="people-wrapper">
-      <Navbar isLoggedIn={isLoggedIn} />
       <div style={backgroundStyle} className="people-top-container">
         <div className="people-top-container-top">
           <div>
-            {personDetails.profile_path !== null && !isLoading ? (
+            {personDetails.profile_path !== null ? (
               <img
                 className="people-img"
                 src={`https://image.tmdb.org/t/p/w500/${personDetails.profile_path}`}
@@ -226,117 +240,112 @@ export default function People() {
               <img className="people-img" src={noFace} alt="not-found" />
             )}
           </div>
-          {!isLoading && (
-            <div>
-              <h1>{personDetails.name}</h1>
-              {personDetails.gender !== 0 && (
-                <p>
-                  {personDetails.gender === 1
-                    ? "Female"
-                    : personDetails.gender === 2
-                    ? "Male"
-                    : "Non-binary"}
-                </p>
-              )}
-              {personDetails.birthday !== null && (
-                <p>
-                  {currentTime.getFullYear() -
-                    personDetails.birthday?.split("-")[0]}{" "}
-                  years old (
-                  {`${personDetails.birthday?.split("-")[2]}-${
-                    personDetails.birthday?.split("-")[1]
-                  }-${personDetails.birthday?.split("-")[0]}`}
-                  )
-                </p>
-              )}
-              {personDetails.deathday !== null && (
-                <p>
-                  {`${personDetails.birthday?.split("-")[2]}-${
-                    personDetails.birthday?.split("-")[1]
-                  }-${personDetails.birthday?.split("-")[0]}`}{" "}
-                  -{" "}
-                  {`${personDetails.deathday?.split("-")[2]}-${
-                    personDetails.deathday?.split("-")[1]
-                  }-${personDetails.deathday?.split("-")[0]}`}{" "}
-                  ({" "}
-                  {personDetails.deathday?.split("-")[0] -
-                    personDetails.birthday?.split("-")[0]}{" "}
-                  years old )
-                </p>
-              )}
-              <p>{personDetails.known_for_department}</p>
-            </div>
-          )}
+
+          <div>
+            <h1>{personDetails.name}</h1>
+            {personDetails.gender !== 0 && (
+              <p>
+                {personDetails.gender === 1
+                  ? "Female"
+                  : personDetails.gender === 2
+                  ? "Male"
+                  : "Non-binary"}
+              </p>
+            )}
+            {personDetails.birthday !== null && (
+              <p>
+                {currentTime.getFullYear() -
+                  personDetails.birthday?.split("-")[0]}{" "}
+                years old (
+                {`${personDetails.birthday?.split("-")[2]}-${
+                  personDetails.birthday?.split("-")[1]
+                }-${personDetails.birthday?.split("-")[0]}`}
+                )
+              </p>
+            )}
+            {personDetails.deathday !== null && (
+              <p>
+                {`${personDetails.birthday?.split("-")[2]}-${
+                  personDetails.birthday?.split("-")[1]
+                }-${personDetails.birthday?.split("-")[0]}`}{" "}
+                -{" "}
+                {`${personDetails.deathday?.split("-")[2]}-${
+                  personDetails.deathday?.split("-")[1]
+                }-${personDetails.deathday?.split("-")[0]}`}{" "}
+                ({" "}
+                {personDetails.deathday?.split("-")[0] -
+                  personDetails.birthday?.split("-")[0]}{" "}
+                years old )
+              </p>
+            )}
+            <p>{personDetails.known_for_department}</p>
+          </div>
         </div>
       </div>
 
-      {!isLoading && (
-        <div className="people-more-info-wrapper">
-          {personDetails.biography !== "" && (
-            <>
-              <h1 className="people-section-title">Biography</h1>
-              <p>{personDetails.biography}</p>
-            </>
+      <div className="people-more-info-wrapper">
+        {personDetails.biography !== "" && (
+          <>
+            <h1 className="people-section-title">Biography</h1>
+            <p>{personDetails.biography}</p>
+          </>
+        )}
+
+        {in_cast_series?.length > 0 ||
+        in_crew_series?.length > 0 ||
+        in_cast_movies?.length > 0 ||
+        in_crew_movies?.length > 0 ? (
+          <h1 className="people-section-title">Known for</h1>
+        ) : (
+          <h2 className="people-section-title no-other-content">
+            Sorry, there is no more available data for {param_person_name}
+          </h2>
+        )}
+
+        {(in_cast_series?.length > 0 || in_crew_series?.length > 0) &&
+          personKnownFor !== null && (
+            <div>
+              <h2>
+                Series ({in_cast_series?.length + in_crew_series?.length})
+              </h2>
+
+              {in_cast_series?.length > 0 && (
+                <div className="knownFor-container">
+                  <p>IN CAST ({in_cast_series?.length})</p>
+                  <div className="known-cast">{in_cast_series}</div>
+                </div>
+              )}
+              {in_crew_series?.length > 0 && (
+                <div>
+                  <p>IN CREW ({in_crew_series?.length})</p>
+                  <div className="known-cast">{in_crew_series}</div>
+                </div>
+              )}
+            </div>
           )}
 
-          {in_cast_series?.length > 0 ||
-          in_crew_series?.length > 0 ||
-          in_cast_movies?.length > 0 ||
-          in_crew_movies?.length > 0 ? (
-            <h1 className="people-section-title">Known for</h1>
-          ) : (
-            <h2 className="people-section-title no-other-content">
-              Sorry, there is no more available data for {personDetails.name}
-            </h2>
+        {(in_cast_movies?.length > 0 || in_crew_movies?.length > 0) &&
+          personKnownFor !== null && (
+            <div>
+              <h2>
+                Movies ({in_cast_movies?.length + in_crew_movies?.length})
+              </h2>
+
+              {in_cast_movies?.length > 0 && (
+                <div className="knownFor-container">
+                  <p>IN CAST ({in_cast_movies?.length})</p>
+                  <div className="known-cast">{in_cast_movies}</div>
+                </div>
+              )}
+              {in_crew_movies?.length > 0 && (
+                <div>
+                  <p>IN CREW ({in_crew_movies?.length})</p>
+                  <div className="known-cast">{in_crew_movies}</div>
+                </div>
+              )}
+            </div>
           )}
-
-          {(in_cast_series.length > 0 || in_crew_series.length > 0) &&
-            personKnownFor !== null && (
-              <div>
-                <h2>
-                  Series ({in_cast_series?.length + in_crew_series?.length})
-                </h2>
-
-                {in_cast_series?.length > 0 && (
-                  <div className="knownFor-container">
-                    <p>IN CAST ({in_cast_series?.length})</p>
-                    <div className="known-cast">{in_cast_series}</div>
-                  </div>
-                )}
-                {in_crew_series?.length > 0 && (
-                  <div>
-                    <p>IN CREW ({in_crew_series?.length})</p>
-                    <div className="known-cast">{in_crew_series}</div>
-                  </div>
-                )}
-              </div>
-            )}
-
-          {(in_cast_movies.length > 0 || in_crew_movies.length > 0) &&
-            personKnownFor !== null && (
-              <div>
-                <h2>
-                  Movies ({in_cast_movies?.length + in_crew_movies?.length})
-                </h2>
-
-                {in_cast_movies?.length > 0 && (
-                  <div className="knownFor-container">
-                    <p>IN CAST ({in_cast_movies?.length})</p>
-                    <div className="known-cast">{in_cast_movies}</div>
-                  </div>
-                )}
-                {in_crew_movies?.length > 0 && (
-                  <div>
-                    <p>IN CREW ({in_crew_movies?.length})</p>
-                    <div className="known-cast">{in_crew_movies}</div>
-                  </div>
-                )}
-              </div>
-            )}
-        </div>
-      )}
-
-      <Footer />
+      </div>
     </div>
   )
 }
