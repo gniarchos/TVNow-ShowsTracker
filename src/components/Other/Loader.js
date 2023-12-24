5fcf1936845376a182cc206f95187400cb2c9915
gniarchos <giannisnrch@gmail.com>
2023-12-24 20:18:18 +0200

    renamed loading text

--- src/components/Other/Loader.js
+++ src/components/Other/Loader.js
@@ -6,7 +6,7 @@ export default function Loader() {
   return (
     <div className="loader-div-global">
       <PuffLoader color={"white"} size={100} />
-      <h2>Just a moment... </h2>
+      <h2>Just a moment </h2>
       <p>Popcorns on the way!</p>
     </div>
   )
