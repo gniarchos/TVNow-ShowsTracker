67fd84450aaa7787e1b5d5bd268cd86049adea1a
Giannis <giannisnrch@gmail.com>
2024-04-28 18:55:42 +0300

    updated loader style

--- src/components/Other/Loader.js
+++ src/components/Other/Loader.js
@@ -5,8 +5,8 @@ import "../../App.css"
 export default function Loader() {
   return (
     <div className="loader-div-global">
-      <PuffLoader color={"white"} size={100} />
-      <h2>Just a moment </h2>
+      <PuffLoader color={"white"} size={80} />
+      <h3>Just a moment </h3>
       <p>Popcorns on the way!</p>
     </div>
   )
