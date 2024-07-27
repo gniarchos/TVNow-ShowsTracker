fe1d1717013f4c59873478f7d1c83270422a3049
Giannis Niarchos <giannisnrch@gmail.com>
2024-07-27 20:00:07 +0300

    updated loader

--- src/components/Other/Loader.js
+++ src/components/Other/Loader.js
@@ -1,13 +1,17 @@
 import React from "react"
 import PuffLoader from "react-spinners/PuffLoader"
 import "../../App.css"
+import { Icon } from "@iconify/react/dist/iconify.js"
 
 export default function Loader() {
   return (
     <div className="loader-div-global">
       <PuffLoader color={"white"} size={80} />
       <h3>Just a moment </h3>
-      <p>Popcorns on the way!</p>
+      <p>
+        Hold tight, the popcorn's almost ready!{" "}
+        <Icon icon="fluent-emoji:popcorn" />
+      </p>
     </div>
   )
 }
