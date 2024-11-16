d2706efa9be8577317e1940e4ff240bb7fbf56b4
Giannis Niarchos <giannisnrch@gmail.com>
2024-11-16 18:40:37 +0200

    fixed shows page crashing for logged out users

--- src/components/Show/Show.js
+++ src/components/Show/Show.js
@@ -33,45 +33,60 @@ export default function Show() {
     useContext(LayoutContext)
 
   useEffect(() => {
-    Promise.all([
-      apiCaller({
-        url: `${process.env.REACT_APP_THEMOVIEDB_URL}/tv/${param_show_id}?api_key=${process.env.REACT_APP_THEMOVIEDB_API}&language=en-US&append_to_response=external_ids,videos,aggregate_credits,content_ratings,recommendations,similar,watch/providers,images`,
-        method: "GET",
-        contentType: "application/json",
-        body: null,
-        calledFrom: "showInfo",
-        isResponseJSON: true,
-        extras: null,
-      }),
-      apiCaller({
-        url: `${process.env.REACT_APP_THEMOVIEDB_URL}/tv/${param_show_id}/season/${seasonNumber}?api_key=${process.env.REACT_APP_THEMOVIEDB_API}&language=en-US`,
-        method: "GET",
-        contentType: "application/json",
-        body: null,
-        calledFrom: "seasonInfo",
-        isResponseJSON: true,
-        extras: null,
-      }),
-      apiCaller({
-        url: `${process.env.REACT_APP_BACKEND_API_URL}/users/${user_id}/all-shows`,
-        method: "GET",
-        contentType: "application/json",
-        body: null,
-        calledFrom: "allUserShows",
-        isResponseJSON: true,
-        extras: null,
-      }),
-    ])
-      .then((data) => {
+    const fetchAPIs = async () => {
+      try {
+        const promises = [
+          apiCaller({
+            url: `${process.env.REACT_APP_THEMOVIEDB_URL}/tv/${param_show_id}?api_key=${process.env.REACT_APP_THEMOVIEDB_API}&language=en-US&append_to_response=external_ids,videos,aggregate_credits,content_ratings,recommendations,similar,watch/providers,images`,
+            method: "GET",
+            contentType: "application/json",
+            body: null,
+            calledFrom: "showInfo",
+            isResponseJSON: true,
+            extras: null,
+          }),
+          apiCaller({
+            url: `${process.env.REACT_APP_THEMOVIEDB_URL}/tv/${param_show_id}/season/${seasonNumber}?api_key=${process.env.REACT_APP_THEMOVIEDB_API}&language=en-US`,
+            method: "GET",
+            contentType: "application/json",
+            body: null,
+            calledFrom: "seasonInfo",
+            isResponseJSON: true,
+            extras: null,
+          }),
+        ]
+
+        // Add the user-specific API call only if the user is logged in
+        if (user_id) {
+          promises.push(
+            apiCaller({
+              url: `${process.env.REACT_APP_BACKEND_API_URL}/users/${user_id}/all-shows`,
+              method: "GET",
+              contentType: "application/json",
+              body: null,
+              calledFrom: "allUserShows",
+              isResponseJSON: true,
+              extras: null,
+            })
+          )
+        }
+
+        // Wait for all resolved promises
+        const data = await Promise.all(promises)
+
         setShowData(data[0])
         setSeasonInfo(data[1])
-        setAllUserShows(data[2])
-      })
-      .catch((error) => {
+        if (user_id) {
+          setAllUserShows(data[2])
+        }
+      } catch (error) {
         setOpenSnackbar(true)
         setSnackbarMessage(error.message)
         setSnackbarSeverity("error")
-      })
+      }
+    }
+
+    fetchAPIs()
   }, [seasonNumber, location])
 
   useEffect(() => {
