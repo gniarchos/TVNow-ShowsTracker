52465e72cb66743f923c186d0a82f079a11c7682
Giannis Niarchos <giannisnrch@gmail.com>
2024-11-30 20:11:12 +0200

    updated to handle actions on new upcoming series on watchlist

--- src/components/Profile/ProfileEpisodes/ProfileEpisodes.js
+++ src/components/Profile/ProfileEpisodes/ProfileEpisodes.js
@@ -83,6 +83,9 @@ export default function ProfileEpisodes({
   return (
     <div className="profile-episode-wrapper">
       <div className="profile-episode-img-container">
+        {showInfo.last_episode_to_air === null && (
+          <span className="profile-episode-new-series">NEW SERIES</span>
+        )}
         <img
           className="profile-episode-img"
           src={`https://image.tmdb.org/t/p/w500/${showInfo.backdrop_path}`}
@@ -123,52 +126,76 @@ export default function ProfileEpisodes({
           </span>
         </div>
 
-        <div className="profile-episode-action">
-          {sectionType !== "upToDate" ? (
-            spinnerLoader.length !== 0 && !spinnerLoader[index] ? (
-              <IconButton
-                onClick={() => {
-                  handleMarkAsWatched(
-                    showInfo.id,
-                    seasonNumber,
-                    episodeNumber,
-                    index
-                  )
-                }}
+        {showInfo.last_episode_to_air !== null ? (
+          <div className="profile-episode-action">
+            {sectionType !== "upToDate" ? (
+              spinnerLoader.length !== 0 && !spinnerLoader[index] ? (
+                <IconButton
+                  onClick={() => {
+                    handleMarkAsWatched(
+                      showInfo.id,
+                      seasonNumber,
+                      episodeNumber,
+                      index
+                    )
+                  }}
+                >
+                  <CheckCircleRoundedIcon sx={{ fontSize: 30 }} />
+                </IconButton>
+              ) : (
+                <ColorRing
+                  visible={true}
+                  height="35"
+                  width="35"
+                  ariaLabel="color-ring-loading"
+                  wrapperStyle={{ marginTop: "5px", marginRight: "5px" }}
+                  wrapperClass="color-ring-wrapper"
+                  colors={[
+                    "#e15b64",
+                    "#f47e60",
+                    "#f8b26a",
+                    "#abbd81",
+                    "#849b87",
+                  ]}
+                />
+              )
+            ) : seasonInfo !== null ? (
+              <span
+                className={
+                  dayjs(seasonInfo?.episodes[episodeNumber].air_date).diff(
+                    dayjs(),
+                    "day"
+                  ) === 0 ||
+                  dayjs(seasonInfo?.episodes[episodeNumber].air_date).format(
+                    "DD-MM-YYYY"
+                  ) === dayjs().format("DD-MM-YYYY")
+                    ? "profile-episode-days-until rainbow rainbow_text_animated"
+                    : "profile-episode-days-until"
+                }
               >
-                <CheckCircleRoundedIcon sx={{ fontSize: 30 }} />
-              </IconButton>
+                {showDaysUntil()}
+              </span>
             ) : (
-              <ColorRing
-                visible={true}
-                height="35"
-                width="35"
-                ariaLabel="color-ring-loading"
-                wrapperStyle={{ marginTop: "5px", marginRight: "5px" }}
-                wrapperClass="color-ring-wrapper"
-                colors={["#e15b64", "#f47e60", "#f8b26a", "#abbd81", "#849b87"]}
-              />
-            )
-          ) : seasonInfo !== null ? (
-            <span
-              className={
-                dayjs(seasonInfo?.episodes[episodeNumber].air_date).diff(
-                  dayjs(),
-                  "day"
-                ) === 0 ||
-                dayjs(seasonInfo?.episodes[episodeNumber].air_date).format(
-                  "DD-MM-YYYY"
-                ) === dayjs().format("DD-MM-YYYY")
-                  ? "profile-episode-days-until rainbow rainbow_text_animated"
-                  : "profile-episode-days-until"
-              }
-            >
-              {showDaysUntil()}
-            </span>
-          ) : (
-            <span className="profile-episode-days-until">TBA</span>
-          )}
-        </div>
+              <span className="profile-episode-days-until">TBA</span>
+            )}
+          </div>
+        ) : (
+          <span
+            className={
+              dayjs(seasonInfo?.episodes[episodeNumber].air_date).diff(
+                dayjs(),
+                "day"
+              ) === 0 ||
+              dayjs(seasonInfo?.episodes[episodeNumber].air_date).format(
+                "DD-MM-YYYY"
+              ) === dayjs().format("DD-MM-YYYY")
+                ? "profile-episode-days-until rainbow rainbow_text_animated"
+                : "profile-episode-days-until"
+            }
+          >
+            {showDaysUntil()}
+          </span>
+        )}
       </div>
     </div>
   )
